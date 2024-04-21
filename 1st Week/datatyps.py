d35d75d23ee3b81dfef4953e1f7498b50c63252e
numeshj <numeshjayamanne@gmail.com>
2024-04-21 13:16:38 +0530

    Update

--- 1st Week/datatyps.py
+++ 1st Week/datatyps.py
@@ -32,6 +32,63 @@ a = "eliphant"
 
 b=0.0
 
-print(type(b))
+# print(type(b))
 
-print("Gifthub Configer")
+# print("Gifthub Configer")
+
+# List
+
+l1 = [1,2,3,4,"Numesh",0.0]
+
+# print(l1)
+
+# print(l1[0:4])
+
+# print(len(l1))
+
+# tuple 
+
+tuple = (1,2,3,4,)
+
+# print(tuple)
+
+is_created = False
+is_failed = True
+
+# print(is_created,type(is_created))
+
+if 2 >0:
+    is_created=True
+    
+# print(is_created)
+
+set1 = set([1,2,3,3,"Numesh", "Numesh"])
+
+# print (set1)
+
+dict={}
+dict1={
+    "Name":"Numesh",
+    "Age" : 34,
+    "Add" : "Galle"   
+}
+
+dict2 ={
+        "Name":"Janith",
+    "Age" : 30,
+    "Add" : "Unawatuna"   
+}
+
+
+# print(dict1["Age"])
+list = []
+
+list.append(dict1)
+list.append(dict2)
+# print(list)
+
+import pandas as pd
+
+df = pd.DataFrame(list)
+print(df.head())
+print(type(df))
