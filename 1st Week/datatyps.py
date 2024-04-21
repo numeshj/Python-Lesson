9f1bd1122ec11fa8540feba5e375ef590961a3d7
numeshj <numeshjayamanne@gmail.com>
2024-04-21 11:42:05 +0530

    trying for datatyps

---
+++ 1st Week/datatyps.py
@@ -0,0 +1,35 @@
+x = "Python{}"
+y = '\n Hello World----'
+
+
+z = """\n Hello World
+this we call doc strings
+we can put paragraphs"""
+
+# print(x)
+
+# Checked the type
+
+# print(type(x))
+
+
+# print(type(x),x)
+
+# print(x,y,z)
+
+# print(x[4])
+# print(x[-2])
+
+a = "eliphant"
+
+# print(type(a))
+
+# print(x.format(a))
+
+# print(x+y+str(a))
+
+# print(f"Numesh {a}")
+
+b=0.0
+
+print(type(b))
